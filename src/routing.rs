4ae26644279ad382e240f52a6ab5f18cbb8ff05a
Joshua Grierson <jgrierson101@gmail.com>
2020-10-11 15:53:16 +0100

    [serialize] - added json serializing

--- src/routing.rs
+++ src/routing.rs
@@ -1,17 +1,37 @@
 use std::fmt;
+use serde::{Serialize};
+use serde_json;
+
+#[derive(Serialize, Debug)]
+enum ErrorType {
+    InvalidRoute
+}
+
+#[derive(Serialize, Debug)]
+struct ErrorResponse {
+    error_type: ErrorType,
+    message: String
+}
 
 #[derive(Debug)]
-pub struct RoutingError;
+pub struct RoutingError {
+    error: ErrorResponse
+}
 
 impl fmt::Display for RoutingError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.write_str("Invalid route")
+        write!(f, "{}", serde_json::to_string(&self.error).unwrap())
     }
 }
 
 pub fn process_route(route: &str) -> Result<bool, RoutingError> {
     match route {
         "/lists" => Ok(true),
-        _ => Err(RoutingError)
+        _ => Err(RoutingError {
+            error: ErrorResponse {
+                error_type: ErrorType::InvalidRoute,
+                message: format!("Route {} not found", route.to_string())
+            }
+        })
     }
 }
